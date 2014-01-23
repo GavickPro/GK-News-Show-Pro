83418d4b14a20e8090e4771a14736a09cd61f39d
dziudek <dziudek@gmail.com>
2014-01-23 22:40:15 +0100

    Fixed problem with unexisting keys in the JSON and XML files

--- gk-nsp/data_sources/xml/layout-parts.php
+++ gk-nsp/data_sources/xml/layout-parts.php
@@ -37,140 +37,172 @@ class GK_NSP_Layout_Parts_xml {
 	 **/
 	 
 	 function art_title($i, $only_value = false) {
-	 	$art_title = $this->parent->wdgt_results[$i]['title'];
-	 	$art_url = $this->parent->wdgt_results[$i]['URL'];
-	 	
-	 	$art_title_short = GK_NSP_Widget_Helpers::cut_text('article_title', $art_title, $this->parent->config['article_title_len_type'], $this->parent->config['article_title_len']);
-	 	
-	 	if($only_value) {
-	 		return apply_filters('gk_nsp_art_raw_title', $art_title_short);
-	 	}
+	 	if(
+	 		isset($this->parent->wdgt_results[$i]['title']) &&
+	 		isset($this->parent->wdgt_results[$i]['URL'])
+	 	) {
+		 	$art_title = $this->parent->wdgt_results[$i]['title'];
+		 	$art_url = $this->parent->wdgt_results[$i]['URL'];
+		 	
+		 	$art_title_short = GK_NSP_Widget_Helpers::cut_text('article_title', $art_title, $this->parent->config['article_title_len_type'], $this->parent->config['article_title_len']);
+		 	
+		 	if($only_value) {
+		 		return apply_filters('gk_nsp_art_raw_title', $art_title_short);
+		 	}
 
-	 	$output = '<h3 class="gk-nsp-header"><a href="'.$art_url.'" title="'.esc_attr(strip_tags($art_title)).'">'.$art_title_short.'</a></h3>';
-	 	
-	 	return apply_filters('gk_nsp_art_title', $output);
+		 	$output = '<h3 class="gk-nsp-header"><a href="'.$art_url.'" title="'.esc_attr(strip_tags($art_title)).'">'.$art_title_short.'</a></h3>';
+		 	
+		 	return apply_filters('gk_nsp_art_title', $output);
+	 	} else {
+	 		return false;
+	 	}
 	 }
 	 
 	 function art_text($i, $only_value = false) {
-	 	$art_text = $this->parent->wdgt_results[$i]['text'];
-	 	
-	 	$art_text = GK_NSP_Widget_Helpers::cut_text('article_text', $art_text, $this->parent->config['article_text_len_type'], $this->parent->config['article_text_len']);
-	 	$art_text = preg_replace('@\[.+?\]@mis', '', $art_text);
-	 	
-	 	if($only_value) {
-	 		return apply_filters('gk_nsp_art_raw_text', $art_text);
-	 	}
+	 	if(isset($this->parent->wdgt_results[$i]['text'])) {
+		 	$art_text = $this->parent->wdgt_results[$i]['text'];
+		 	
+		 	$art_text = GK_NSP_Widget_Helpers::cut_text('article_text', $art_text, $this->parent->config['article_text_len_type'], $this->parent->config['article_text_len']);
+		 	$art_text = preg_replace('@\[.+?\]@mis', '', $art_text);
+		 	
+		 	if($only_value) {
+		 		return apply_filters('gk_nsp_art_raw_text', $art_text);
+		 	}
 
-	 	$output = '<p class="gk-nsp-text">'.$art_text.'</p>';
-	 	
-	 	return apply_filters('gk_nsp_art_text', $output);
+		 	$output = '<p class="gk-nsp-text">'.$art_text.'</p>';
+		 	
+		 	return apply_filters('gk_nsp_art_text', $output);
+	 	} else {
+	 		return false;
+	 	}
 	 }
 	 
 	 function art_image($i, $only_value = false) {
- 	 	$art_title = $this->parent->wdgt_results[$i]['title'];
-	 	$art_url = $this->parent->wdgt_results[$i]['URL'];
-	 	$art_image = $this->parent->wdgt_results[$i]['image'];
-	 	
-	 	if($art_image != '') {
-	 		$style = '';
+ 	 	if(
+	 		isset($this->parent->wdgt_results[$i]['title']) &&
+	 		isset($this->parent->wdgt_results[$i]['image']) &&
+	 		isset($this->parent->wdgt_results[$i]['URL'])
+	 	) {
+	 	 	$art_title = $this->parent->wdgt_results[$i]['title'];
+		 	$art_url = $this->parent->wdgt_results[$i]['URL'];
+		 	$art_image = $this->parent->wdgt_results[$i]['image'];
 		 	
-	 		if($only_value) {
-	 			return apply_filters('gk_nsp_art_raw_image', $art_image);
-	 		}
+		 	if($art_image != '') {
+		 		$style = '';
+			 	
+		 		if($only_value) {
+		 			return apply_filters('gk_nsp_art_raw_image', $art_image);
+		 		}
 
-		 	if($this->parent->config['image_block_padding'] != '' && $this->parent->config['image_block_padding'] != '0') {
-		 		$style = ' style="margin: '.$this->parent->config['image_block_padding'].';"';
+			 	if($this->parent->config['image_block_padding'] != '' && $this->parent->config['image_block_padding'] != '0') {
+			 		$style = ' style="margin: '.$this->parent->config['image_block_padding'].';"';
+			 	}
+
+			 	// if the popup is enabled
+	 			$link_additional_classes = '';
+	 			$link_rel = '';
+	 			if($this->parent->config['article_image_popup'] == 'on') {
+	 				$art_url = $art_image;
+	 				$link_additional_classes = ' thickbox';
+	 				$link_rel = ' rel="gallery-gk-nsp-' . $this->parent->id . '"';
+	 			}
+			 		
+	 			if($this->parent->config['article_image_pos'] == 'left' && $this->parent->config['article_image_order'] == 1) {
+	 				$output = '<div class="gk-nsp-image-wrap"><a href="'.$art_url.'" title="'.esc_attr(strip_tags($art_title)).'" class="gk-image-link'.$link_additional_classes.'"'.$style.$link_rel.'><img src="'.$art_image.'" alt="" class="gk-nsp-image" width="'.$this->parent->config['article_image_w'].'" height="'.$this->parent->config['article_image_h'].'" /></a></div>';
+
+	 				return apply_filters('gk_nsp_art_image', $output);
+	 			} else {
+	 				$output = '<a href="'.$art_url.'" title="'.esc_attr(strip_tags($art_title)).'" class="gk-responsive gk-image-link'.$link_additional_classes.'"'.$style.$link_rel.'><img src="'.$art_image.'" alt="" class="gk-nsp-image gk-responsive" width="'.$this->parent->config['article_image_w'].'" height="'.$this->parent->config['article_image_h'].'" /></a>';
+
+	 				return apply_filters('gk_nsp_art_image', $output);
+	 			}
+		 	} else {
+		 		return '';
 		 	}
-
-		 	// if the popup is enabled
- 			$link_additional_classes = '';
- 			$link_rel = '';
- 			if($this->parent->config['article_image_popup'] == 'on') {
- 				$art_url = $art_image;
- 				$link_additional_classes = ' thickbox';
- 				$link_rel = ' rel="gallery-gk-nsp-' . $this->parent->id . '"';
- 			}
-		 		
- 			if($this->parent->config['article_image_pos'] == 'left' && $this->parent->config['article_image_order'] == 1) {
- 				$output = '<div class="gk-nsp-image-wrap"><a href="'.$art_url.'" title="'.esc_attr(strip_tags($art_title)).'" class="gk-image-link'.$link_additional_classes.'"'.$style.$link_rel.'><img src="'.$art_image.'" alt="" class="gk-nsp-image" width="'.$this->parent->config['article_image_w'].'" height="'.$this->parent->config['article_image_h'].'" /></a></div>';
-
- 				return apply_filters('gk_nsp_art_image', $output);
- 			} else {
- 				$output = '<a href="'.$art_url.'" title="'.esc_attr(strip_tags($art_title)).'" class="gk-responsive gk-image-link'.$link_additional_classes.'"'.$style.$link_rel.'><img src="'.$art_image.'" alt="" class="gk-nsp-image gk-responsive" width="'.$this->parent->config['article_image_w'].'" height="'.$this->parent->config['article_image_h'].'" /></a>';
-
- 				return apply_filters('gk_nsp_art_image', $output);
- 			}
-	 	} else {
-	 		return '';
-	 	} 
+		} else {
+			return false;
+		} 
 	 }
 	 
 	 function art_info($i) {
 	 	$item = $this->parent->wdgt_results[$i];
 	 	// replacements for the possible tags
-	 	$category = $item['categories'];
+	 	$category = isset($item['categories']) ? $item['categories'] : '';
 	 	$author = '';
 	 	$date = '';
 	 	$stars = '';
 	 	$comments = '';
-	 	$comment_count = $item['comment_count'];
+	 	$comment_count = isset($item['comment_count']) ? $item['comment_count'] : '';
 	 	
  		
 	 	// check if there is a author in format
 	 	if(stripos($this->parent->config['article_info_format'], '{AUTHOR}') !== FALSE) {	 			 		
-	 		$author_username = $item['author_name'];
-	 		$author_url = $item['author_URL'];
-	 		$author = '<a href="'.$author_url.'" class="gk-nsp-author">'.$author_username.'</a>';
+	 		if(
+	 			isset($item['author_name']) &&
+	 			isset($item['author_URL'])
+	 		) {
+	 			$author_username = $item['author_name'];
+	 			$author_url = $item['author_URL'];
+	 			$author = '<a href="'.$author_url.'" class="gk-nsp-author">'.$author_username.'</a>';
+	 		}	
 	 	}
 	 	// check if there is a date in format
 	 	if(stripos($this->parent->config['article_info_format'], '{DATE}') !== FALSE) {
-	 		$date = '<span class="gk-nsp-date">' . date($this->parent->config['article_info_date_format'], strtotime($item['date'])) . '</span>';
+	 		if(isseT($item['date'])) {
+	 			$date = '<span class="gk-nsp-date">' . date($this->parent->config['article_info_date_format'], strtotime($item['date'])) . '</span>';
+	 		}
 	 	}
 	 	// check if there are the stars in format
 	 	if(stripos($this->parent->config['article_info_format'], '{STARS}') !== FALSE) {
-	 		$rating = $item['rating'];
-	 		if(isset($rating) && trim($rating) != '') {
-	 			$rating = explode('/', $rating);
-	 			if(count($rating) == 2) {
-	 				if(
-	 					is_numeric(trim($rating[0])) && 
-	 					is_numeric(trim($rating[1])) && 
-	 					trim($rating[0]) * 1 <= trim($rating[1]) * 1 &&
-	 					trim($rating[0]) * 1 >= 0 && trim($rating[1]) > 0
-	 				) {
-	 					$rate = trim($rating[0]) * 1;
-	 					$total = trim($rating[1]) * 1;
+	 		$rating = isset($item->rating) ? $item->rating : '';
+	 		if(isset($item['rating'])) {
+		 		if(isset($rating) && trim($rating) != '') {
+		 			$rating = explode('/', $rating);
+		 			if(count($rating) == 2) {
+		 				if(
+		 					is_numeric(trim($rating[0])) && 
+		 					is_numeric(trim($rating[1])) && 
+		 					trim($rating[0]) * 1 <= trim($rating[1]) * 1 &&
+		 					trim($rating[0]) * 1 >= 0 && trim($rating[1]) > 0
+		 				) {
+		 					$rate = trim($rating[0]) * 1;
+		 					$total = trim($rating[1]) * 1;
 
-	 					$stars = '<span class="gk-nsp-stars">';
-	 					for($i = 0; $i < $total; $i++) {
-							$stars .= $i < $rate ? '<span class="gk-nsp-star-1"></span>' : '<span class="gk-nsp-star-0"></span>';
-						}
-						$stars .= '</span>';
-	 				} else {
-	 					$stars = 'Wrong rating data received';
-	 				}
-	 			} else {
-	 				$stars = 'Wrong rating data received';
-	 			}
-	 		} else {
-	 			$stars = 'Not rated yet';
+		 					$stars = '<span class="gk-nsp-stars">';
+		 					for($i = 0; $i < $total; $i++) {
+								$stars .= $i < $rate ? '<span class="gk-nsp-star-1"></span>' : '<span class="gk-nsp-star-0"></span>';
+							}
+							$stars .= '</span>';
+		 				} else {
+		 					$stars = 'Wrong rating data received';
+		 				}
+		 			} else {
+		 				$stars = 'Wrong rating data received';
+		 			}
+		 		} else {
+		 			$stars = 'Not rated yet';
+		 		}
 	 		}
 	 	}
 	 	// check if there is a comments in format
 	 	if(stripos($this->parent->config['article_info_format'], '{COMMENTS}') !== FALSE) {
-	 		$comment_phrase = '';
+	 		if(isset($item['comment_count'])) {
+		 		$comment_phrase = '';
 
-	 		if($comment_count == 0) {
-	 			$comment_phrase = __('No comments', 'gk-nsp');
-	 		} else if($comment_count >= 1) {
-	 			$comment_phrase = __('Comments ', 'gk-nsp') . '(' . $comment_count . ')';
+		 		if($comment_count == 0) {
+		 			$comment_phrase = __('No comments', 'gk-nsp');
+		 		} else if($comment_count >= 1) {
+		 			$comment_phrase = __('Comments ', 'gk-nsp') . '(' . $comment_count . ')';
+		 		}
+
+		 		$comments = '<a href="'.$item['comment_link'].'#comments">'.$comment_phrase.'</a>';
 	 		}
-
-	 		$comments = '<a href="'.$item['comment_link'].'#comments">'.$comment_phrase.'</a>';
 	 	}
 	 	// check if there is a comments_count in format
 	 	if(stripos($this->parent->config['article_info_format'], '{COMMENT_COUNT}') !== FALSE) {
-	 		$comment_count = '<a href="'.$item['comment_link'].'#comments" class="gk-nsp-comment-count">' .$comment_count. '</a>';
+	 		if(isset($item['comment_count'])) {
+	 			$comment_count = '<a href="'.$item['comment_link'].'#comments" class="gk-nsp-comment-count">' .$comment_count. '</a>';
+	 		}
 	 	}
 	 	// replace them all!
 	 	$output = str_replace(
@@ -183,15 +215,19 @@ class GK_NSP_Layout_Parts_xml {
 	 }
 	 
 	 function art_readmore($i, $only_value = false) {
-	 	$art_url = $this->parent->wdgt_results[$i]['URL'];
-	 	
-	 	if($only_value) {
-	 		return apply_filters('gk_nsp_art_raw_readmore', $art_url);
-	 	}
+	 	if(isset($this->parent->wdgt_results[$i]['URL'])) {
+		 	$art_url = $this->parent->wdgt_results[$i]['URL'];
+		 	
+		 	if($only_value) {
+		 		return apply_filters('gk_nsp_art_raw_readmore', $art_url);
+		 	}
 
-	 	$output = '<a href="'.$art_url.'" class="readon btn" title="'.__('Read more', 'gk-nsp').'">'.__('Read more', 'gk-nsp').'</a>';
-	 	
-	 	return apply_filters('gk_nsp_art_readmore', $output);
+		 	$output = '<a href="'.$art_url.'" class="readon btn" title="'.__('Read more', 'gk-nsp').'">'.__('Read more', 'gk-nsp').'</a>';
+		 	
+		 	return apply_filters('gk_nsp_art_readmore', $output);
+	 	} else {
+	 		return false;
+	 	}
 	 }
 	 
 	 /**
@@ -201,25 +237,36 @@ class GK_NSP_Layout_Parts_xml {
 	  **/
 	  
 	function link_title($i) {
-		$art_title = $this->parent->wdgt_results[$i]['title'];
-		$art_url = $this->parent->wdgt_results[$i]['URL'];
-		
-		$art_title_short = GK_NSP_Widget_Helpers::cut_text('links_title', $art_title, $this->parent->config['links_title_len_type'], $this->parent->config['links_title_len']);
-		
-		$output = '<h4 class="gk-nsp-link-header"><a href="'.$art_url.'" title="'.esc_attr(strip_tags($art_title)).'">'.$art_title_short.'</a></h4>';
-		
-		return apply_filters('gk_nsp_link_title', $output);
+		if(
+	 		isset($this->parent->wdgt_results[$i]['title']) &&
+	 		isset($this->parent->wdgt_results[$i]['URL'])
+	 	) {
+			$art_title = $this->parent->wdgt_results[$i]['title'];
+			$art_url = $this->parent->wdgt_results[$i]['URL'];
+			
+			$art_title_short = GK_NSP_Widget_Helpers::cut_text('links_title', $art_title, $this->parent->config['links_title_len_type'], $this->parent->config['links_title_len']);
+			
+			$output = '<h4 class="gk-nsp-link-header"><a href="'.$art_url.'" title="'.esc_attr(strip_tags($art_title)).'">'.$art_title_short.'</a></h4>';
+			
+			return apply_filters('gk_nsp_link_title', $output);
+		} else {
+			return false;
+		}
 	}
 
 	function link_text($i) {
-		$art_text = $this->parent->wdgt_results[$i]['text'];
-		
-		$art_text = GK_NSP_Widget_Helpers::cut_text('links_text', $art_text, $this->parent->config['links_text_len_type'], $this->parent->config['links_text_len']);
-		$art_text = preg_replace('@\[.+?\]@mis', '', $art_text);
-		
-		$output = '<p class="gk-nsp-link-text">'.$art_text.'</p>';
-		
-		return apply_filters('gk_nsp_link_text', $output);
+		if(isset($this->parent->wdgt_results[$i]['text'])) {
+			$art_text = $this->parent->wdgt_results[$i]['text'];
+			
+			$art_text = GK_NSP_Widget_Helpers::cut_text('links_text', $art_text, $this->parent->config['links_text_len_type'], $this->parent->config['links_text_len']);
+			$art_text = preg_replace('@\[.+?\]@mis', '', $art_text);
+			
+			$output = '<p class="gk-nsp-link-text">'.$art_text.'</p>';
+			
+			return apply_filters('gk_nsp_link_text', $output);
+		} else {
+			return false;
+		}
 	}
 }
 
