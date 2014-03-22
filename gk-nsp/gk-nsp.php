68a6314a4a864d8d8d60e90735e16ca73feabda0
dziudek <dziudek@gmail.com>
2014-03-22 21:56:59 +0100

    Preparing for the relese - date and version.

--- gk-nsp/gk-nsp.php
+++ gk-nsp/gk-nsp.php
@@ -4,7 +4,7 @@
 Plugin Name: GK News Show Pro
 Plugin URI: http://www.gavick.com/
 Description: Advanced widget for the posts display
-Version: beta
+Version: 1.0.0
 Author: GavickPro
 Author URI: http://www.gavick.com/
 License: GPLv2 or later
